1b98358b68fa44e0ebba0bdd49e8665691cbd6ab
Khodor Wahab <akapeith@gmail.com>
2026-02-18 14:32:28 +0200

    sorted categories

--- app/api/categories/get/route.ts
+++ app/api/categories/get/route.ts
@@ -2,11 +2,17 @@ import { NextResponse } from "next/server";
 import { supabaseChicken } from "@/lib/supabase";
 export const dynamic = 'force-dynamic';
 export async function GET() {
+    const sortedCategories =[
+  "Appetizer", "Sandwich", "Burger", "Chicken", 
+  "Meals", "Wings", "Offers", "Sauce", "Drinks"
+];
     const { data, error } = await supabaseChicken
       .from("categories")
       .select(`id, title, description, image_url, products(id)`)
       .order("created_at", { ascending: false });
-      
+    const sortedData = data?.sort((a, b) => {
+  return sortedCategories.indexOf(a.title) - sortedCategories.indexOf(b.title);
+});
     if (error) return NextResponse.json({ error: error.message }, { status: 500 });
-    return NextResponse.json(data);
+    return NextResponse.json(sortedData);
 }
