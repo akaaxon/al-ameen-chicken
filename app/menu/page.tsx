73573f6acc673402944616ba2357578e997d9063
axon-Devs-CEO <akaaxon@outlook.com>
2026-07-03 14:04:57 +0300

    made categories on mobile stack in rows in menu page

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -208,7 +208,7 @@ function MenuGrid() {
             </div>
           </div>
 
-          <div className="flex gap-2 overflow-x-auto no-scrollbar pb-2">
+          <div className="flex flex-wrap gap-2 pb-2">
             {categories.map((cat) => (
               <button
                 key={cat.id}
