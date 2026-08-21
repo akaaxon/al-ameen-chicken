6381123250b84149f8a755567eb686704237eb27
akaaxon <akapeith@gmail.com>
2026-08-21 15:12:29 +0300

    og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,28 @@ const cairo = Cairo({
   weight: ["400", "700", "900"],
   variable: "--font-cairo",
 });
+
 export const metadata: Metadata = {
   title: "فروج الأمين",
   description: "أطيب فروج مشوي",
+  openGraph: {
+    title: "فروج الأمين",
+    description: "أطيب فروج مشوي",
+    images: [
+      {
+        url: "/og-image.png", // The path to your image
+        width: 500,
+        height: 500,
+        alt: "فروج الأمين - أطيب فروج مشوي",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "فروج الأمين",
+    description: "أطيب فروج مشوي",
+    images: ["/og-image.png"], // The path to your image
+  },
 };
 
 export default function RootLayout({
